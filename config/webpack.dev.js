41ccbbd7804fd93c5c6da036cc096d6fa0fec4db
metatron020NaNa03 <malikmanan97@gmail.com>
2020-12-10 22:18:01 +0530

    first-commit-webpack

---
+++ config/webpack.dev.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  entry: {
+    main: "./src/main.js"
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist")
+  }
+};
